beadab33e7fd818ab11dfcbf8fd527f473d08532
Pascal <pascal@pascal.com>
2022-12-12 10:18:19 -0500

    add createContext to list of import names

--- src/react-hooks-import-validation.js
+++ src/react-hooks-import-validation.js
@@ -14,6 +14,7 @@ module.exports = {
                 const name = node.imported.name;
 
                 if (
+                    name === "createContext" ||
                     name === "useState" ||
                     name === "useEffect" ||
                     name === "useContext" ||
